55d55fe20ad7d84a2e0f626c6c9715f24431c2ff
oberch85 <ober.ch@gmail.com>
2026-02-03 17:35:07 -0500

    linut py6

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -138,8 +138,9 @@ def add_review(request):
         data = json.loads(request.body)
         try:
             response = post_review(data)
-            return JsonResponse({"status":200})
-        except:
-            return JsonResponse({"status":401,"message":"Error in posting review"})
+            return JsonResponse({"status": 200})
+        except Exception as e:
+            return JsonResponse({"status": 401, 
+                                 "message": "Error in posting review"})
     else:
-        return JsonResponse({"status":403,"message":"Unauthorized"})
+        return JsonResponse({"status": 403, "message": "Unauthorized"})
